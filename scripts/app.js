993d801ddd06f5cfe75117158642556b4d77570d
Alexey Lezov <koloss0005@gmail.com>
2024-01-24 17:30:07 +0300

    Update_24.01

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+/*function calculate() {
+    // Получаем значения из полей формы
+    var propertyType = document.getElementById('propertyType').value;
+    var registrationDate = new Date(document.getElementById('registrationDate').value);
+
+    // Здесь вы можете добавить свои вычисления на основе полученных значений
+
+    // Выводим результат на страницу
+    document.getElementById('result').innerText = 'Результат: ...';
+}
+*/
