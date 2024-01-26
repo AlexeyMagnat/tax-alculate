57c0e58406d962c43e500e5b9d8dbc24f3ba43a2
Alexey Lezov <koloss0005@gmail.com>
2024-01-26 17:48:45 +0300

    Up_3

--- scripts/app.js
+++ scripts/app.js
@@ -26,6 +26,34 @@ function saveData() {
 	localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
 }
 
+/* render */
+function rerenderMenu(activeHabit) {
+	if (!activeHabit) {
+		return;
+	}
+	//   document.querySelector(".menu__list")
+	for (const habit of habits) {
+		existed = document.querySelector(`[menu-habit-id="${habit.id}"]`);
+		if (!existed) {
+			// создание
+			const element = document.createElement('button');
+		}
+		if (activeHabit.id === habit.id) {
+			existed.classList.add('menu__item_active');
+		} else {
+			existed.classList.remove('menu__item_active');
+		}
+	}
+}
+
+function rerender(activeHabitId) {
+	const activeHabit = habits.find((habit) => habit.id === activeHabitId);
+	rerenderMenu(activeHabit);
+}
+
+/* init */
+
 (() => {
 	loadData();
+	rerender(habits[0].id);
 })();
