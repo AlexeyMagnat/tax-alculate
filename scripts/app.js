694db7e1003206ab2a7f5f3716402df4c6933de6
Alexey Lezov <koloss0005@gmail.com>
2024-01-26 17:13:02 +0300

    Save_js

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,23 @@
     document.getElementById('result').innerText = 'Результат: ...';
 }
 */
+let habits = [];
+const HABIT_KEY = 'HABIT_KEY';
+
+/* utils */
+
+function loadData() {
+	const habitsString = localStorage.getItem('HABIT_KEY');
+	const habitArray = JSON.parse(habitsString);
+	if (Array.isArray(habitArray)) {
+		habits = habitArray;
+	}
+}
+
+function saveData() {
+	localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+}
+
+(() => {
+	loadData();
+})();
